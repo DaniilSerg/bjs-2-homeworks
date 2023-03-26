3b451608f48326c7e16b3b8c82b5b03f35bb6cc0
DaniilSerg <119966955+DaniilSerg@users.noreply.github.com>
2023-03-26 20:39:08 +0300

    First change in homework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,19 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+    let arr = [];
+    let d;
+    let k;
+    d = b**-4*a*c;
+    if (d === 0) {
+       k = -b/(2*a);
+       arr.push(k);
+    } else if (d > 0) {
+       k = (-b + Math.sqrt(d) )/(2*a);
+       arr.push(k);
+       k = (-b - Math.sqrt(d) )/(2*a);
+       arr.push(k);
+    }
+    return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
