69da650677160554df6b211b2171b06857cce78c
DaniilSerg <119966955+DaniilSerg@users.noreply.github.com>
2023-05-17 21:05:00 +0300

    перезагрузка

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -13,26 +13,27 @@ Student.prototype.addMarks = function(...marks) {
 	if (this.hasOwnProperty('marks') === false) {
 		return console.log("Студент отчислен");
 	} else {
+
 		this.marks.push(...marks);
 	}
 }
 
 Student.prototype.getAverage = function() {
 	if (this.hasOwnProperty('marks') === false || this.marks.length === 0) {
-		this.getAverage = 0;
+		this.Average = 0;
 	} else {
 		sum = this.marks.reduce(
 			(accumulator, currentValue) => accumulator + currentValue
 		);
 		avg = +(sum / this.marks.length).toFixed(2);
-		this.getAverage = avg;
+		this.Average = avg;
 	}
+	return this.Average;
 }
 
 Student.prototype.exclude = function(reason) {
 	delete this.marks;
-	delete this.getAverage;
+	delete this.Average;
 	this.excluded = reason;
 
 }
-
