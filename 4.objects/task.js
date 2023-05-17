fe78933fd8c14b3d25ce9cc7f265843759338899
DaniilSerg <119966955+DaniilSerg@users.noreply.github.com>
2023-05-17 20:32:38 +0300

    Чтобы победить

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -19,20 +19,20 @@ Student.prototype.addMarks = function(...marks) {
 
 Student.prototype.getAverage = function() {
 	if (this.hasOwnProperty('marks') === false || this.marks.length === 0) {
-		this.average = 0;
+		this.getAverage = 0;
 	} else {
 		sum = this.marks.reduce(
 			(accumulator, currentValue) => accumulator + currentValue
 		);
 		avg = +(sum / this.marks.length).toFixed(2);
-		this.average = avg;
+		this.getAverage = avg;
 	}
 }
 
 Student.prototype.exclude = function(reason) {
 	delete this.marks;
-	delete this.average;
-	this.exclude = reason;
+	delete this.getAverage;
+	this.excluded = reason;
 
 }
 
